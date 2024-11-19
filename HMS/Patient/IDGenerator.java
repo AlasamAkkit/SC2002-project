db9b0cf4c8300c30f1a9d5eab8a6906b45d655a9
qd <chuaqindi123@gmail.com>
2024-11-19 21:35:18 +0800

    changed idGenerator

--- HMS/Patient/IDGenerator.java
+++ HMS/Patient/IDGenerator.java
@@ -3,14 +3,16 @@ package HMS.Patient;
 import java.util.Set;
 
 public class IDGenerator implements IDGeneratorInterface {
-    private static Set<String> existingIDs;
+    private Set<String> existingIDs;
+    private int patientCount;
 
     public IDGenerator(Set<String> existingIDs) {
         this.existingIDs = existingIDs;
+        this.patientCount = existingIDs.size();
     }
 
     public String generateUniqueID() {
-        int patientCount = existingIDs.size();
+        //int patientCount = existingIDs.size();
         String patientID;
         do {
             patientCount++;
