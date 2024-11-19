04cfda16249446b3e6e5cc05d95f39fd26378939
julianlf2 <leowisaac@gmail.com>
2024-11-19 21:55:52 +0800

    a

--- HMS/Patient/IDGenerator.java
+++ HMS/Patient/IDGenerator.java
@@ -4,20 +4,21 @@ import java.util.Set;
 
 public class IDGenerator implements IDGeneratorInterface {
     private Set<String> existingIDs;
-    private int patientCount;
+    private static int patientCount = 0;
 
     public IDGenerator(Set<String> existingIDs) {
         this.existingIDs = existingIDs;
-        this.patientCount = existingIDs.size();
     }
 
     public String generateUniqueID() {
         //int patientCount = existingIDs.size();
+        System.out.println(existingIDs);
         String patientID;
         do {
             patientCount++;
             patientID = "P" + String.format("%04d", patientCount);
         } while (existingIDs.contains(patientID));
+        existingIDs.add(patientID);
         return patientID;
     }
 }
