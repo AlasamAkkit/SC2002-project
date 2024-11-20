be1236cbdb57402818e46c40705ecd727a23558f
julianlf2 <leowisaac@gmail.com>
2024-11-20 14:30:20 +0800

    Update Validator.java

--- HMS/Utility/Validator.java
+++ HMS/Utility/Validator.java
@@ -14,28 +14,4 @@ public interface Validator {
     boolean isValid(String input);
 }
 
-/*class EmailValidator implements Validator {
-    @Override
-    public boolean isValid(String input) {
-        String emailRegex = "^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$";
-        return input.matches(emailRegex);
-    }
-}
-
-/**
- * Validates if the provided string consists only of numeric characters.
- */
-class NumericValidator implements Validator {
-
-    /**
-     * Validates that the string contains only digits.
-     *
-     * @param input The string to validate.
-     * @return true if the string consists solely of numeric characters, false otherwise.
-     */
-    @Override
-    public boolean isValid(String input) {
-        return input.matches("\\d+");
-    }
-}*/
 
